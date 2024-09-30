0ff094af174d240f374491ac06bb497e38137f9f
fe2-Nyxar <153492661+fe2-Nyxar@users.noreply.github.com>
2024-09-30 17:51:11 +0100

    Update telescope.lua
    added some features that might help, for the git_files() and find_files()

--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -11,8 +11,27 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
+        -- search is limited to the current directory only
+        vim.keymap.set('n', '<leader>pp', function()
+            local Cdir = vim.fn.expand('%:p')
+                if Cdir == "" then
+                    builtin.find_files()
+                else
+                    Cdir = string.gsub(Cdir, "oil://", "");
+                    Cdir = string.gsub(Cdir, "[^/]+%.%w+$", "");
+                    builtin.find_files({ cwd = Cdir })
+                end
+            end, {})
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        -- look for .git folder, remove the error when using nvim /path
+        vim.keymap.set('n', '<C-p>', function()
+            local git_dir = vim.fn.fnamemodify(vim.fn.finddir(".git", ".;"), ":h")
+                if git_dir == "" then
+                    builtin.git_files()
+                else
+                    builtin.git_files({ cwd = vim.fn.getcwd() .. "/" .. git_dir })
+                end
+            end, {})
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
