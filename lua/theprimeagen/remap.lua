7d8a10e15e8438313774c37dff91bd4dddf96867
fe2-Nyxar <153492661+fe2-Nyxar@users.noreply.github.com>
2024-09-30 21:26:36 +0100

    Update remap.lua
    autoformat when saving a file :)

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -56,3 +56,14 @@ vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
 
+-- auto format when saving a file
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+        local mode = vim.api.nvim_get_mode().mode
+        local filetype = vim.bo.filetype
+        if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+            vim.cmd('lua vim.lsp.buf.format()')
+        else
+        end
+    end
+})
